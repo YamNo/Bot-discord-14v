51c593d8fe506e49946f1e29fcba6136e7ab5c0d
Arfah <112329704+YamNo@users.noreply.github.com>
2026-01-20 22:02:51 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    DISCORD_TOKEN: " ",
+    OWNER_ID: "",
+    MONGO_URI: "",
+    LOG_WEBHOOK_URL: "https://discord.com/api/webhooks/1463186945441267855/kvc7Rj8gtqHvUCDhd89D7a-dpwZs4cVyTaWzO1USEGR-RVSjmC0J7I3wMdEnC-Dlo_Ho" // Isi dengan Webhook URL kamu
+};
